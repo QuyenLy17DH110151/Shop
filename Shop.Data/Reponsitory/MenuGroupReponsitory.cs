edc87eebff1ecccece1dbf6dcec1b9cd55ee800b
QUYEN LY <quyen.ly.151@outlook.com>
2021-03-22 22:56:57 +0700

    add Responsitory

---
+++ Shop.Data/Reponsitory/MenuGroupReponsitory.cs
@@ -0,0 +1,17 @@
+﻿using Shop.Data.Infrastructure;
+using Shop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Reponsitory
+{
+    public class MenuGroupReponsitory : RepositoryBase<MenuGroup>
+    {
+        public MenuGroupReponsitory(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ Shop.Data/Reponsitory/ProductCategoryReponsitory.cs
@@ -0,0 +1,26 @@
+﻿using Shop.Data.Infrastructure;
+using Shop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Reponsitory
+{
+    public interface IProductCategoryReponsitory
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+    public class ProductCategoryReponsitory : RepositoryBase<ProductCategory>,IProductCategoryReponsitory
+    {
+        public ProductCategoryReponsitory(IDbFactory dbFactory):base(dbFactory)
+        {
+        }
+
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            return DbContext.ProductCategories.Where(x => x.Alias == alias);
+        }
+    }
+}
